6f5d025f8b68de24f0f771e36bdeca43cdf942e4
thillo <salysem90@gmail.com>
2024-08-06 14:11:11 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script >
+import { computed, reactive, ref } from 'vue';
+
+export default{
+      setup() {
+        const newTodo = ref('')
+        const todos = ref([])
+        function addNewTodo(){
+          // console.log(newTodo.value);
+          todos.value.push({
+            done: false,
+            content: newTodo.value
+          })
+        }
+        return {
+          todos,
+          newTodo,
+          addNewTodo,
+        };
+      }
+    }
+
+
+</script>
+
+<template>
+  <h1>Vue 3 Todo App</h1>
+   <form @submit.prevent="addNewTodo">
+    <label >New Todo</label>
+    <input v-model="newTodo" name="newTodo">
+    <button>Add New Todo</button>
+  </form> 
+  <div v-for="todo in todos">
+    
+    <h3>{{ todo.content }}</h3>
+
+  </div>
+  
+</template>
+
+<style scoped>
+
+
+</style>
